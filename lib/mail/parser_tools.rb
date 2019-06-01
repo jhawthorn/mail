a2f7db2f0ee7e0e87eab3d467c335874ede07c39
John Hawthorn <john.hawthorn@gmail.com>
2019-06-01 16:23:57 -0700

    Use Array8/Array16 to optimize ragel arrays

--- lib/mail/parser_tools.rb
+++ lib/mail/parser_tools.rb
@@ -12,7 +12,48 @@ module Mail
       end
     end
 
+    RAGEL_VARS = [:@_trans_keys, :@_key_spans, :@_index_offsets, :@_indicies,
+                  :@_trans_targs, :@_trans_actions, :@_eof_actions]
+
+    class Array8
+      def initialize(arr)
+        @str = arr.pack("c*").freeze
+      end
+
+      def [](idx)
+        @str.getbyte(idx)
+      end
+    end
+
+    class Array16
+      def initialize(arr)
+        @low  = arr.map{ |x| x & 0xff }.pack("c*").freeze
+        @high = arr.map{ |x| x >> 8 }.pack("c*").freeze
+      end
+
+      def [](idx)
+        (@high.getbyte(idx) << 8) | @low.getbyte(idx)
+      end
+    end
+
     def optimize_ragel_data!
+      (instance_variables & RAGEL_VARS).each do |var|
+        array = instance_variable_get(var)
+        max = array.max
+
+        next if array.size <= 8
+
+        array =
+          if max < 2**8
+            Array8.new(array)
+          elsif max < 2**16
+            Array16.new(array)
+          else
+            array
+          end
+
+        instance_variable_set(var, array)
+      end
     end
   end
 end
